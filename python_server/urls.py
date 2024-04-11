f0fba5b0f245c4caba8b2af72d12a548970d7c30
Ermiyas Bitew <ermiyasbitew239@gmail.com>
2024-04-11 16:54:43 +0300

    removed api from path

--- python_server/urls.py
+++ python_server/urls.py
@@ -2,13 +2,13 @@ from django.urls import include, path
 from dashboard.api import app
 
 urlpatterns = [
-    path('api/', include('dashboard.urls.airtime_urls')),
-    path('api/', include('dashboard.urls.equb_urls')),
-    path('api/', include('dashboard.urls.institution_urls')),
-    path('api/', include('dashboard.urls.invitation_urls')),
-    path('api/', include('dashboard.urls.recurring_contract_urls')),
-    path('api/', include('dashboard.urls.saving_urls')),
-    path('api/', include('dashboard.urls.transaction_urls')),
-    path('api/', include('dashboard.urls.transfer_urls')),
-    path('api/', include('dashboard.urls.user_urls')),
+    path('', include('dashboard.urls.airtime_urls')),
+    path('', include('dashboard.urls.equb_urls')),
+    path('', include('dashboard.urls.institution_urls')),
+    path('', include('dashboard.urls.invitation_urls')),
+    path('', include('dashboard.urls.recurring_contract_urls')),
+    path('', include('dashboard.urls.saving_urls')),
+    path('', include('dashboard.urls.transaction_urls')),
+    path('', include('dashboard.urls.transfer_urls')),
+    path('', include('dashboard.urls.user_urls')),
 ]
