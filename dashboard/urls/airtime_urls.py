ab7e5e6761ed412772fd5eae2a00000a9faeae4c
Ermiyas Bitew <ermiyasbitew239@gmail.com>
2024-05-07 11:08:03 +0300

    list endpoints path changed

--- dashboard/urls/airtime_urls.py
+++ dashboard/urls/airtime_urls.py
@@ -3,5 +3,5 @@ from ..views import airtime_views
 
 urlpatterns = [
     path('airtime/buy', airtime_views.proxy_buy_airtime, name='buy-airtime'),
-    path('airtime/', airtime_views.proxy_list_recharges, name='list-recharges'),
+    path('airtime/list', airtime_views.proxy_list_recharges, name='list-recharges'),
 ]
--- dashboard/urls/transfer_urls.py
+++ dashboard/urls/transfer_urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from ..views import transfer_views
 
 urlpatterns = [
-    path('transfer', transfer_views.proxy_get_transfer_list, name='get-transfer-list'),
+    path('transfer/list', transfer_views.proxy_get_transfer_list, name='get-transfer-list'),
     path('transfer/send', transfer_views.proxy_transfer_as_user, name='transfer-as-user'),
     path('transfer/lookup-external', transfer_views.proxy_external_account_lookup, name='external-account-lookup'),
     path('transfer/fee', transfer_views.proxy_get_transfer_fee, name='get-transfer-fee'),
