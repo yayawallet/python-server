b3fc010ca341f852bdafce05676eb40ab1714fe4
Ermiyas Bitew <ermiyasbitew239@gmail.com>
2024-05-22 00:00:04 +0300

    archive url fix

--- dashboard/urls/scheduled_urls.py
+++ dashboard/urls/scheduled_urls.py
@@ -4,5 +4,5 @@ from ..views import scheduled_views
 urlpatterns = [
     path('scheduled-payment/create', scheduled_views.proxy_create_schedule, name='create-schedule'),
     path('scheduled-payment/list', scheduled_views.proxy_schedule_list, name='schedule-list'),
-    path('scheduled-payment/archive/', scheduled_views.proxy_archive_schedule, name='archive-schedule'),
+    path('scheduled-payment/archive/<str:id>', scheduled_views.proxy_archive_schedule, name='archive-schedule'),
 ]
