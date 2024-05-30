384a1275f4c00e4a9be64124dc3d6b99fce47400
Ermiyas Bitew <ermiyasbitew239@gmail.com>
2024-05-30 13:48:25 +0300

    urls changed

--- dashboard/urls/auth_urls.py
+++ dashboard/urls/auth_urls.py
@@ -7,6 +7,6 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
