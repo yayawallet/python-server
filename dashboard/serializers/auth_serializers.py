75b44e5748006b34489f96ef020ce34ac10e728f
Ermiyas Bitew <ermiyasbitew239@gmail.com>
2024-05-31 15:51:47 +0300

    token expiry extended

--- dashboard/serializers/auth_serializers.py
+++ dashboard/serializers/auth_serializers.py
@@ -6,7 +6,6 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super().get_token(user)
         
-        # Add custom claims
         token['user_id'] = user.id
         token['username'] = user.username
         
