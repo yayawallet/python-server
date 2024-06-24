3f84b2c9909d38c86c0bc18800b7b5d202337243
Ermiyas Bitew <ermiyasbitew@hennessey.com>
2024-06-24 08:58:50 +0300

    save api keys on user creation

--- dashboard/serializers/auth_serializers.py
+++ dashboard/serializers/auth_serializers.py
@@ -8,6 +8,8 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         
         token['user_id'] = user.id
         token['username'] = user.username
+        token['roles'] = list(user.groups.values_list('name', flat=True))
+        token['permissions'] = list(user.get_group_permissions())
         
         return token
 
