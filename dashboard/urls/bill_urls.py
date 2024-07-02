a7293a76f73d41e84460f7178fba64c227cceb7b
Ermiyas Bitew <ermiyasbitew239@gmail.com>
2024-07-02 09:41:47 +0300

    added remaining bill endpoints

--- dashboard/urls/bill_urls.py
+++ dashboard/urls/bill_urls.py
@@ -4,6 +4,8 @@ from ..views import bill_views
 urlpatterns = [
     path('bill/create', bill_views.proxy_create_bill, name='create-bill'),
     path('bulkimport/bills', bill_views.proxy_create_bulk_bill, name='create-bulk-bill'),
-    path('bulkimport/', bill_views.proxy_bulk_bill_status, name='bulk-bill-status'),
+    path('bulkimport/list', bill_views.proxy_bulk_bill_status, name='bulk-bill-status'),
     path('bill/update', bill_views.proxy_update_bill, name='update-bill'),
+    path('bill/list', bill_views.proxy_bill_list, name='bill-list'),
+    path('bill/find', bill_views.proxy_bill_find, name='bill-find'),
 ]
