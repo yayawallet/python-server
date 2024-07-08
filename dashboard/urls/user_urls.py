cf3a196c03d16ba33fb86d5c14c6e8dc2abc7533
Ermiyas Bitew <ermiyasbitew239@gmail.com>
2024-07-08 15:50:32 +0300

    added agent role endpoints

--- dashboard/urls/user_urls.py
+++ dashboard/urls/user_urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('user/organization', user_views.proxy_get_organization, name='get-organization'),
     path('user/profile', user_views.proxy_get_user, name='get-profile'),
     path('user/search', user_views.proxy_search_user, name='get-profile'),
+    path('user/register', user_views.proxy_create_customer_user, name='create-customer'),
+    path('user/register-business', user_views.proxy_create_business_user, name='create-business'),
 ]
--- dashboard/views/user_views.py
+++ dashboard/views/user_views.py
@@ -2,6 +2,7 @@ from adrf.decorators import api_view
 from rest_framework.response import Response
 from yayawallet_python_sdk.api import user
 from .stream_response import stream_response
+from ..permisssions.async_permission import async_permission_required
 
 @api_view(['GET'])
 async def proxy_get_organization(request):
@@ -17,4 +18,18 @@ async def proxy_get_user(request):
 async def proxy_search_user(request):
     data = request.data
     response = await user.search_user(data.get('query'))
+    return stream_response(response)
+
+@async_permission_required('auth.create_user', raise_exception=True)
+@api_view(['POST'])
+async def proxy_create_customer_user(request):
+    data = request.data
+    response = await user.create_customer_user(data)
+    return stream_response(response)
+
+@async_permission_required('auth.create_user', raise_exception=True)
+@api_view(['POST'])
+async def proxy_create_business_user(request):
+    data = request.data
+    response = await user.create_business_user(data)
     return stream_response(response)
