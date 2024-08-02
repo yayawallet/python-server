48c78f038443dfa0888381f3c7b9c746762a7c08
Ermiyas Bitew <ermiyasbitew239@gmail.com>
2024-08-02 10:41:26 +0300

    token life type decreased

--- python_server/settings.py
+++ python_server/settings.py
@@ -81,7 +81,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=2),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
