b72039dd83b7e27d1112a4b3133844d4822e073d
Ermiyas Bitew <ermiyasbitew239@gmail.com>
2024-08-22 10:49:52 +0300

    single scheduled approval added

--- dashboard/management/commands/add_approver_permissions.py
+++ dashboard/management/commands/add_approver_permissions.py
@@ -10,11 +10,21 @@ class Command(BaseCommand):
         group, _ = Group.objects.get_or_create(name="Approver")
 
         approval_bulk_schedule_import = Permission.objects.get(codename="approval_bulk_schedule_import")
+        bulk_scheduled_requests = Permission.objects.get(codename="bulk_scheduled_requests")
         approval_bulk_contract_import = Permission.objects.get(codename="approval_bulk_contract_import")
+        bulk_contract_requests = Permission.objects.get(codename="bulk_contract_requests")
         approval_bulk_payment_request_import = Permission.objects.get(codename="approval_bulk_payment_request_import")
+        bulk_requested_payments = Permission.objects.get(codename="bulk_requested_payments")
+        approval_scheduled = Permission.objects.get(codename="approval_scheduled")
+        scheduled_requests = Permission.objects.get(codename="scheduled_requests")
 
         group.permissions.add(approval_bulk_schedule_import)
+        group.permissions.add(bulk_scheduled_requests)
         group.permissions.add(approval_bulk_contract_import)
+        group.permissions.add(bulk_contract_requests)
         group.permissions.add(approval_bulk_payment_request_import)
+        group.permissions.add(bulk_requested_payments)
+        group.permissions.add(approval_scheduled)
+        group.permissions.add(scheduled_requests)
 
         self.stdout.write(self.style.SUCCESS('Successfully added permissions to Approver group'))
--- dashboard/urls/scheduled_urls.py
+++ dashboard/urls/scheduled_urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from ..views import scheduled_views
 
 urlpatterns = [
-    path('scheduled-payment/create', scheduled_views.proxy_create_schedule, name='create-schedule'),
+    path('scheduled-payment/schedule-request', scheduled_views.schedule_request, name='schedule-request'),
+    path('scheduled-payment/submit-scheduled-response', scheduled_views.submit_scheduled_response, name='submit-scheduled-response'),
+    path('scheduled-payment/scheduled-requests', scheduled_views.scheduled_requests, name='scheduled-requests'),
+    path('scheduled-payment/scheduled-my-requests', scheduled_views.scheduled_my_requests, name='scheduled-my-requests'),
     path('scheduled-payment/list', scheduled_views.proxy_schedule_list, name='schedule-list'),
     path('scheduled-payment/archive/<str:id>', scheduled_views.proxy_archive_schedule, name='archive-schedule'),
     path('scheduled-payment/bulk-schedule-import-request', scheduled_views.bulk_schedule_import_request, name='bulk-schedule-import-request'),
